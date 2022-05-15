dd284a614469dbe77a5230cbefa090c4af8b6c21
Meliksah Gurtemel <92724453+meliksahgurtemel@users.noreply.github.com>
2022-05-15 22:41:46 +0300

    mock cToken contract

---
+++ contracts/test/TestcToken.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.10;
+
+import "./TestERC20.sol";
+
+contract TestcToken is TestERC20 {
+
+    uint public exchangeRate;
+
+    constructor(string memory _name, string memory _symbol, uint8 _decimals) TestERC20(_name, _symbol, _decimals) public {}
+
+    function setExchangeRate(uint _exchangeRate) public {
+        exchangeRate = _exchangeRate;
+    }
+
+    function exchangeRateCurrent() public returns (uint) {
+        return exchangeRate;
+    }
+
+    function balanceOfUnderlying(address account) public returns (uint) {
+        return balanceOf(account);
+    }
+}
